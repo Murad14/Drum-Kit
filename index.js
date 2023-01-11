f0a80693bac4dc07c656eaa2ed21423374482034
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-11 20:58:43 +0600

    sound added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
-
-let btns = document.querySelectorAll('button');
+let btns = document.querySelectorAll('.drum');
 
 for (i of btns) {
   i.addEventListener('click', function() {
-    alert("I got clicked!");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+
   });
 }
+
+
